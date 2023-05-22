b902f47904d9f8b2941e4c47de744a783da3c6ea
Alberto Moreno <amoreno@altracorporacion.es>
2023-05-22 08:49:06 +0200

    closures

---
+++ closures.js
@@ -0,0 +1,25 @@
+const miContador = (function(){
+    let _contador = 0;
+
+    function incrementar(){
+        return _contador++;
+    }
+
+    function decrementar(){
+        return _contador--;
+    }
+
+    function valor(){
+        return _contador;
+    }
+
+    return{
+        incrementar,
+        decrementar,
+        valor
+    }
+})();
+
+miContador.valor()
+miContador.incrementar()
+miContador.valor()
